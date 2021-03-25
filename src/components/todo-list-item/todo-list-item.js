ace9b0997c4f8e3cda6a905161dc8157b70d0cc7
Evgeny Ovodkov <kefir4ek@gmail.com>
2021-03-25 20:44:23 +0300

    ListItem fixes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -16,25 +16,28 @@ export default class TodoListItem extends Component {
     }
   
     return (
-      <span className= { classNames }>
+      <div className= { classNames }>
         <span
           className="todo-list-item-label"
           onClick = { onToggleDone }>
           {label}
         </span>
   
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right"
-                onClick = { onToggleImportant }>
-          <i className="fa fa-exclamation" />
-        </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right"
-                onClick = { onDeleted }>
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
+        <div>
+          <button type="button"
+                  className="btn btn-outline-success btn-sm float-right"
+                  onClick = { onToggleImportant }>
+            <i className="fa fa-exclamation" />
+          </button>
+    
+          <button type="button"
+                  className="btn btn-outline-danger btn-sm float-right"
+                  onClick = { onDeleted }>
+            <i className="fa fa-trash-o" />
+          </button>
+        </div>
+        
+      </div>
     );
   };
 }
